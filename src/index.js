6d89d82c0e2e5eec033972f36fb59c8c92784841
bagrounds <bagrounds@gmail.com>
2016-11-22 15:44:46 -0800

    [PATCH][ADD] input validation

--- src/index.js
+++ src/index.js
@@ -7,9 +7,24 @@
   'use strict'
 
   /* imports */
+  var specifier = require('specifier')
+  var typeCheck = require('type-check')
 
   /* exports */
   module.exports = funTest
+
+  var optionsSpec = {
+    fut: [
+      isFunction
+    ],
+    input: [
+    ],
+    verifier: [
+      isFunction
+    ]
+  }
+
+  var specificationChecker = specifier(optionsSpec)
 
   /**
    * funTest is a simple function tester.
@@ -24,6 +39,13 @@
    * @param {Function} reporter handles verifier results
    */
   function funTest (options, reporter) {
+    var error = specificationChecker(options)
+
+    if (error) {
+      reporter(error)
+      return
+    }
+
     options.fut(options.input, function (error, result) {
       var verifierOptions = {
         error: error,
@@ -32,6 +54,15 @@
 
       options.verifier(verifierOptions, reporter)
     })
+  }
+
+  function isFunction (candidate) {
+    var ok = typeCheck('Function', candidate)
+
+    if (!ok) {
+      var message = 'Should be a function.'
+      return new Error(message)
+    }
   }
 })()
 
