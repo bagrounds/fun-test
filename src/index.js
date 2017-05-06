bc909b152172339228a982b854e4cfb0c33e0a87
bagrounds <bagrounds@gmail.com>
2017-05-06 16:54:06 -0700

    [MINOR][REMOVE] old functions and tests

--- src/index.js
+++ src/index.js
@@ -7,31 +7,10 @@
 
   /* imports */
   var Task = require('data.task')
-  var funTry = require('fun-try')
-  var object = require('fun-object')
   var fn = require('fun-function')
-
-  var merge = object.concat(function (a, b) {
-    return b === undefined ? a : b
-  })
-
-  var defaultConfig = {
-    mapData: id,
-    mapSubject: id,
-    action: function (options) {
-      return Task.of(options.data)
-    },
-    update: function (options) {
-      return options.result
-    },
-    assertion: id
-  }
 
   /* exports */
   module.exports = {
-    concat: concat,
-    of: fn.compose(of, merge(defaultConfig)),
-    empty: empty,
     sync: sync
   }
 
@@ -54,59 +33,6 @@
       (options.action || fn.apply)(options.inputs),
       (options.contra || fn.id)
     ])
-  }
-
-  function concat (t1, t2) {
-    return function (options) {
-      return Task.of(options)
-        .chain(t1)
-        .map(object.of('data'))
-        .map(merge(options))
-        .chain(t2)
-    }
-  }
-
-  /**
-   *
-   * @function module:fun-test.of
-   *
-   * @param {Object} config - all function parameters
-   * @param {Function} config.mapData - Data -> Data
-   * @param {Function} config.mapSubject - Subject -> Subject
-   * @param {Function} config.action - ({ Data, Subject }) -> Task Result
-   * @param {Function} config.assertion - Data -> Data | throws
-   * @param {Function} config.update - ({ Data, Result }) -> Data
-   * @param {String} config.comment - for the reporter
-   * @param {Number} config.timeout - in ms
-   *
-   * @return {Function} ({data, subject, reporter}) -> Task Data
-   */
-  function of (config) {
-    return function (options) {
-      return Task.of(options)
-        .map(object.keep(['data', 'subject']))
-        .map(object.ap({ data: config.mapData, subject: config.mapSubject }))
-        .chain(funTry(config.action))
-        .chain(id)
-        .chain(funTry(config.assertion))
-        .map(object.of('result'))
-        .orElse(fn.compose(Task.of, object.of('error')))
-        .map(merge({ data: options.data, subject: options.subject }))
-        .map(merge(config))
-        .chain(options.reporter)
-        .map(object.keep(['data', 'result', 'error']))
-        .map(config.update)
-    }
-  }
-
-  function empty () {
-    return function (options) {
-      return Task.of()
-    }
-  }
-
-  function id (x) {
-    return x
   }
 })()
 
